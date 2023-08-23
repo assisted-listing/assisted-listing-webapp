4d68639149b8f1c11196747de1e047c0eda49adf
bigmike0009 <mikea0009@gmail.com>
2023-08-23 17:35:22 -0400

    Added result page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IntakeFormComponent } from './intake-form/intake-form.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ResultPageComponent } from './result-page/result-page.component';
 
-const routes: Routes = [{path: '', component: IntakeFormComponent }];
+const routes: Routes = [
+  {path: '', component: LandingPageComponent },
+{path: 'generate', component: IntakeFormComponent},
+{path: 'resTest', component: ResultPageComponent },
+{path: 'result', component: ResultPageComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
